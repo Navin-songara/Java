14a982453a52066b80cf7757b18fbd85ea63e6fd
navin-songara <navinsongara1@gmail.com>
2025-08-27 23:01:24 +0530

    added intro java file

---
+++ intro.java
@@ -0,0 +1,7 @@
+// JAVA :- Java is an object-oriented Programming language 
+
+class Test {
+    public static void main(String[] args) {
+        System.out.println("Java  first program -NS");
+    }
+}
