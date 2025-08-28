290e2a73376064e1c3694a249cc23df431143451
navin-songara <navinsongara1@gmail.com>
2025-08-28 23:22:41 +0530

    Characters = string

---
+++ characters.java
@@ -0,0 +1,11 @@
+public class characters {
+    public static void main(String[] args) {
+        char a = '!';
+        System.out.println("Character Ranges");
+        System.out.println("----------");
+        System.out.println("Minimum value : " + (int) Character.MIN_VALUE); //0
+        System.out.println("Maximum value : " + (int) Character.MAX_VALUE); //65535
+        // Common ASCII examples (0-127 range)
+        System.out.println((char) 10084);
+    }
+}
