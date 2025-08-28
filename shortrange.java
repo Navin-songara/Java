d1acc24dba273f0672e9d5c7c482649b32bc22f2
navin-songara <navinsongara1@gmail.com>
2025-08-28 23:08:17 +0530

    short range

---
+++ shortrange.java
@@ -0,0 +1,12 @@
+public class shortrange {
+    public static void main(String[] args) {
+        System.out.println("Short Range");
+        System.out.println("--------------");
+        short b = 2;
+        System.out.println("Example short value " + b);
+        System.out.println("Minimum short value " + Short.MIN_VALUE); // -32768 (16 bits)
+        System.out.println("Maximum short value " + Short.MAX_VALUE); // 32767
+        System.out.println();
+
+    }
+}
