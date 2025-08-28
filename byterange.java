b45c35f9409f1120bf093770f2f77b3177dd0784
navin-songara <navinsongara1@gmail.com>
2025-08-28 22:55:45 +0530

    byte range

---
+++ byterange.java
@@ -0,0 +1,12 @@
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Byte Range");
+        System.out.println("--------------");
+        byte a = 127;
+        System.out.println("Example byte value " + a);
+        System.out.println("Minimum byte value " + Byte.MIN_VALUE); // -128 (8  bits)
+        System.out.println("Maximum byte value " + Byte.MAX_VALUE); // 127
+        System.out.println();
+
+    }
+}
