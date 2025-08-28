fe6e263a44d47e9039e770d40df074e9270faa98
navin-songara <navinsongara1@gmail.com>
2025-08-28 23:02:09 +0530

    integer range

---
+++ intrange.java
@@ -0,0 +1,12 @@
+public class intrange {
+    public static void main(String[] args) {
+        System.out.println("Integer Range");
+        System.out.println("--------------");
+        int c = 3;
+        System.out.println("Example integer value " + c);
+        System.out.println("Minimum integer value " + Integer.MIN_VALUE); // -2147483648 (32 bits)
+        System.out.println("Maximum integer value " + Integer.MAX_VALUE); // 2147483647
+        System.out.println();
+
+    }
+}
