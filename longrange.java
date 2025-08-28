f96306a05767f84c19a004fa53c87a902f17cc8c
navin-songara <navinsongara1@gmail.com>
2025-08-28 23:06:01 +0530

    long range

---
+++ longrange.java
@@ -0,0 +1,12 @@
+public class longrange {
+    public static void main(String[] args) {
+        System.out.println("Long Range");
+        System.out.println("--------------");
+        long d = 4;
+        System.out.println("Example long value " + d);
+        System.out.println("Minimum long value " + Long.MIN_VALUE); // -9223372036854775808 (64 bits)
+        System.out.println("Maximum long value " + Long.MAX_VALUE); // 9223372036854775807
+        System.out.println();
+
+    }
+}
