57daa0acdde8577dcf1e6851daa2d4553b4e95d2
navin-songara <navinsongara1@gmail.com>
2025-08-29 22:19:33 +0530

    String Object

---
+++ stringobject.java
@@ -0,0 +1,7 @@
+public class stringobject {
+    public static void main(String[] args) {
+        // Jvm --> heap memory allocation // String Pool
+       stringobject str = new stringobject();
+       stringobject str2 = new stringobject();
+    }
+}
